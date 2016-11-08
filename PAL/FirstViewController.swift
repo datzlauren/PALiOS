799cf422546d0ab1d445635f8d0a9d996500bf03
datzlauren <ldatz@seas.upenn.edu>
2016-11-08 11:32:39 -0500

    Initial Commit

---
+++ PAL/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PAL
+//
+//  Created by Lauren Datz on 11/8/16.
+//  Copyright © 2016 happy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PALTests/PALTests.swift
@@ -0,0 +1,36 @@
+//
+//  PALTests.swift
+//  PALTests
+//
+//  Created by Lauren Datz on 11/8/16.
+//  Copyright © 2016 happy. All rights reserved.
+//
+
+import XCTest
+@testable import PAL
+
+class PALTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
